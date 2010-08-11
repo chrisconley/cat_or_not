f359de1c71d99cf5467c2aa052c24be8c9e6dd5a
Chris Conley <chris.m.conley@gmail.com>
2010-08-11 10:26:15 -0400

    more changes

--- app.rb
+++ app.rb
@@ -1,11 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 
-require 'dm-core'
-require 'houdini'
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{File.dirname(__FILE__)}/development.db")
-
 get '/images' do
   @images = Image.all
   haml :index
@@ -17,7 +12,7 @@ get '/images/new' do
 end
 
 post '/images' do
-  @image => Post.new(params[:image])
+  @image = Post.new(params[:image])
   if @image.save
     redirect_to '/images'
   else
@@ -31,6 +26,9 @@ post '/images/:id/houdini_postbacks' do
   @image.save!
 end
 
+require 'dm-core'
+DataMapper.setup(:default, ENV['DATABASE_URL'])
+
 class Image
   include DataMapper::Resource
   property :id,           Integer, :serial=>true
@@ -40,11 +38,26 @@ class Image
   after :create, :moderate_image
 
   def moderate_image
-    Houdini.send_to_houdini(
-    :api_key => Houdini::KEY,
-    :price => '0.01',
-    :title => "Please moderate the image for Frank Sinatra",
-    :form_template => 'view/houdini_templates/image.html.erb',
-    :postback_url => "http://houdini-sinatra-example.com/images/#{id}/houdini_postbacks")
+    Houdini.send_to_houdini({
+      :api_key => 'YOUR_API_KEY',
+      :price => '0.01',
+      :title => "Please moderate the image for Frank Sinatra",
+      :form_html => Houdini.render_form(self, 'view/houdini_templates/image.html.erb'),
+      :postback_url => "http://houdini-sinatra-example.com/images/#{id}/houdini_postbacks"
+    })
+  end
+end
+
+require 'net/http'
+require 'uri'
+
+class Houdini
+  def self.send_to_houdini(params)
+    url = URI.parse("http://houdinihq.com/api/v0/simple/tasks/")
+    response, body = Net::HTTP.post_form(url, params)
+  end
+
+  def self.render_form(object, template)
+    # ...
   end
 end
